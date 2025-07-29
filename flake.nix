9ed69d6dbc1b968e42401d8b7b3757ee4aec65fd
Kurt Bendlin <kurt.bendlin@bdr.de>
2025-07-29 10:51:50 +0200

    feat: add sed

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       in {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = scriptName;
-          version = "1.1.3";
+          version = "1.2.0";
           src = ./.;
           nativeBuildInputs = [ pkgs.makeWrapper pkgs.coreutils ];
           installPhase = ''
@@ -24,7 +24,7 @@
           '';
           postFixup = ''
             wrapProgram $out/bin/${scriptName} \
-              --set PATH ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.kubectl pkgs.kubectx pkgs.docker pkgs.netcat pkgs.gum pkgs.gnugrep pkgs.coreutils pkgs.jq ]}
+              --set PATH ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.kubectl pkgs.kubectx pkgs.docker pkgs.netcat pkgs.gum pkgs.gnugrep pkgs.coreutils pkgs.jq pkgs.sed]}
           '';
         };
 
